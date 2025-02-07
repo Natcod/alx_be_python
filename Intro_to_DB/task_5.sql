114c47f7eaaff71ac908636e32045e468318b417
Natcod <natnaelshetaye1@gmail.com>
2025-02-07 10:34:39 +0300

    INsert a single row into the customers table

---
+++ Intro_to_DB/task_5.sql
@@ -0,0 +1,3 @@
+-- Insert a sing row into the customers table 
+INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+VALUES (1, 'Cole Baido', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
